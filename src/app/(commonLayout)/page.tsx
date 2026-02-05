6227ffbaedaa4184817a0a8bd89c061c97721ff9
almamun528 <ayapths@gmail.com>
2026-02-05 10:20:13 +0600

    fix env

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { userService } from "@/services/user.service";
-// ! Home page
+
 export default async function Home() {
   const { data, error } = await userService.getSelection();
-  console.log(data);
+  if (!error) console.log(data);
   return (
     <div>
       <Button>Click Here</Button>
